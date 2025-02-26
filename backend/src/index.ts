9d3dbbd3154160b30d438303442a8acd5ed7c3ca
miu3u <miu.blue3@gmail.com>
2025-02-26 18:58:24 +0900

    書籍の取得

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,34 +1,41 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
+// テストデータおよび今回扱う書籍管理データの型定義
 type BookManager = {
   id: number;
   name: string;
   status: string;
-}
-
+};
 const bookManager: BookManager[] = [
-  { id: 1, name: 'React入門', status: '在庫あり' },
-  { id: 2, name: 'TypeScript入門', status: '貸出中' },
-  { id: 3, name: 'Next.js入門', status: '返却済' },
+  { id: 1, name: "React入門", status: "在庫あり" },
+  { id: 2, name: "TypeScript入門", status: "貸出中" },
+  { id: 3, name: "Next.js入門", status: "返却済" },
 ];
 
-app.get('/books', (c) => {
-  return c.json(bookManager)
-})
+// キーワード検索した書籍データを取得する
+app.get("/books", async (c) => {
+  // キーワードがクエリにあるなら、そのキーワードを含む書籍データを返す
+  const query = c.req.query();
+  const keyword = query.keyword;
+  if (keyword) {
+    // book.nameにキーワードが含まれるなら残す処理
+    return c.json(bookManager.filter((book) => book.name.includes(keyword)));
+  }
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+  return c.json(bookManager);
+});
 
-const port =  8080;
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+const port = 8080;
 console.log(`Server is running on http://localhost:${port}`);
 
 serve({
   fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+  port,
+});
